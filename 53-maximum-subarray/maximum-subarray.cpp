42f9c337b2575675c06f637ac588c7a7e4880e0a
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-10-07 16:22:46 +0530

    Time: 80 ms (40.81%) | Memory: 70.3 MB (99.72%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN; int ans=0;
+        for(int i=0;i<nums.size();i++){
+             ans+=nums[i];
+             maxi=max(maxi,ans);
+             if(ans<0) ans=0;
+        }
+        return maxi;
+    }
+};
