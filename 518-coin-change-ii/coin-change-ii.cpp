90925c83abc94deacaef817fb06b254b973c7775
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-10-09 18:51:46 +0530

    Time: 946 ms (5.01%) | Memory: 85 MB (5.35%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   int solve(int  amount,vector<int>&nums, int index,vector<vector<int>>&dp){
+        if(amount==0) return 1;
+        if(amount<0)  return 0;
+        if(dp[amount][index]!=-1) return dp[amount][index];
+         int ans1,ans2=0;
+        for(int i=index;i<nums.size();i++){
+            ans2+=solve(amount-nums[i],nums, i ,dp);
+        }
+             dp[amount][index]=ans2;
+           return dp[amount][index];
+   }
+    int change(int amount, vector<int>& coins) {
+       vector<vector<int>>dp(amount+6000,vector<int>(coins.size()+9,-1));
+        return solve(amount,coins,0,dp);
+    }
+};
