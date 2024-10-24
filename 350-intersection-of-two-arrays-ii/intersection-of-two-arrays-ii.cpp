8bf5233f3d1f54860d7ee258cbdb92dedf1d327c
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-10-24 11:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (84.70%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -8,14 +8,9 @@ public:
         int i=0; int j=0;
 
         while(i<nums1.size()&&j<nums2.size()){
+             
              if(nums1[i]==nums2[j]){
-            //     if(ans.size()==0){
-            //         ans.push_back(nums1[i]);
-            //     }
-            //     else if(ans.back()!=nums1[i]){
-            //     ans.push_back(nums1[i]);
-            //     }
-                ans.push_back(nums1[i]);
+               ans.push_back(nums1[i]);
                 i++; j++;
              }
              else if(nums1[i]<nums2[j]){
