9c67bba60bb402d951a2b725a6e2e209b36df855
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-11-05 02:15:15 +0530

    Time: 10 ms (6.42%) | Memory: 9.9 MB (37.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,14 +4,14 @@ public:
     
     if(s.length()!=t.length()) return false;
 
-       unordered_map<char,int>map1,map2;
+       map<char,int>map1,map2;
           for(int i=0;i<s.length();i++){
                   map1[s[i]]++;
             }
           for(int i=0;i<t.length();i++){
             map2[t[i]]++;
           }  
-          
+
     return (map1==map2) ;
     }
 };
