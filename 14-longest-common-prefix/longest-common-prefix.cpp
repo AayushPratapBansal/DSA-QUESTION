94bf6e73d3f73ee2d857a54078288edcf1e8196a
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-11-05 01:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (74.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,12 +3,8 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
      string ans="";
      int mini=INT_MAX;
-       if(strs.size()==0) return " ";
-       for(int i=0;i<strs.size();i++){
-           if(strs[i].size()<mini)
-            mini=strs[i].size();
-       }
-
+    //    if(strs.size()==0) return " ";
+   
     string compare=strs[0];
     for(int i=0;i<compare.size();i++){
         for(int j=1;j<strs.size();j++){
