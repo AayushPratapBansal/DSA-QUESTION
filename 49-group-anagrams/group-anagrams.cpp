87527d13dd1900b8e135a209b203adc06baf1f69
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-11-05 15:28:21 +0530

    Time: 19 ms (58.43%) | Memory: 25.2 MB (52.24%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>m;
+
+        for(auto i:strs){
+             string curr=i;
+             sort(i.begin(),i.end());
+             m[i].push_back(curr);
+        }
+
+        for(auto i:m){
+            ans.push_back(i.second);
+        }
+
+  return ans;
+    }
+};
