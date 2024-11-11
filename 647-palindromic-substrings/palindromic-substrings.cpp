6e15520a66d280dddea76e0f74a408694bf3b520
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-11-11 17:18:57 +0530

    Time: 1384 ms (6.90%) | Memory: 407 MB (10.50%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+  bool check(string s ,int i, int j){
+       while(i<j){
+         if(s[i]==s[j]){
+            i++,j--;
+         }
+         else return false;
+       }
+     return true;
+    }
+
+  
+    int countSubstrings(string s) {
+        int count=0;;
+         for(int i=0;i<s.size();i++){
+         for(int j=i;j<s.size();j++){
+                if(check(s, i , j))
+                    count++;
+         }
+
+      }
+     return count;
+    }
+};
