41254adb7593010cf103bc06f702aa2af2369f2a
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2024-11-28 19:05:52 +0530

    Time: 20 ms (9.25%) | Memory: 19.1 MB (5.34%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) 
+    {
+        int countVowel = 0;
+        for(auto c:s)
+            if(c == 'a' or c == 'e' or c == 'i'
+                or c == 'o' or c == 'u') countVowel++; 
+        return countVowel;
+    }
+};
