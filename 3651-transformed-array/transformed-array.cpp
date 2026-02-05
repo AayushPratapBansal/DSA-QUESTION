726b025aecbf0fb674ff1a8e1536b6918e541b57
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2026-02-05 17:20:03 +0530

    Time: 8 ms (46.52%) | Memory: 25.5 MB (98.89%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+               vector<int> ans(nums.size());
+               int n=nums.size();
+               for(int i=0;i<nums.size();i++){
+                    ans[i]=   nums[(((i+nums[i])%n)+n)%n];
+
+               }
+       return ans;
+    }
+
+};
