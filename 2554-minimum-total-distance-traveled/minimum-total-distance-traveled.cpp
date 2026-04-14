ac7528ae5533ae9acf209b3a0d220095f6859598
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2026-04-15 00:24:25 +0530

    Time: 35 ms (58.22%) | Memory: 13.4 MB (74.66%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    long long dp[100][100][100];
-    long long f(int i, int j, int k, vector<int>& robot, vector<vector<int>>& factory)
-    {
-        if (i<0) return 0;
-        if (j<0) return 1LL<<40;
-        if (dp[i][j][k]!=-1) return dp[i][j][k];
-        int xR=robot[i], xF=factory[j][0];
-        long long otherFactory=f(i, j-1, 0, robot, factory);
-        long long factoryJ=(k<factory[j][1])?abs(xR-xF)+f(i-1, j, k+1, robot, factory):1LL<<40;
-        return dp[i][j][k]=min(otherFactory,  factoryJ);
+    vector<vector<long long>> dp;
 
+    long long solve(int i, int j, vector<int>& robot, vector<vector<int>>& factory) {
+        int n = robot.size();
+        int m = factory.size();
+
+        // all robots assigned
+        if (i == n) return 0;
+
+        // no factory left
+        if (j == m) return 1e15;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        long long res = solve(i, j + 1, robot, factory); // skip factory
+
+        long long cost = 0;
+        int pos = factory[j][0];
+        int limit = factory[j][1];
+
+        // assign k robots
+        for (int k = 0; k < limit && i + k < n; k++) {
+            cost += abs(robot[i + k] - pos);
+            res = min(res, cost + solve(i + k + 1, j + 1, robot, factory));
+        }
+
+        return dp[i][j] = res;
     }
-    void print(auto& c){
-        for(int x:c) cout<<x<<", ";
-        cout<<endl;
-    }
+
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
         sort(robot.begin(), robot.end());
         sort(factory.begin(), factory.end());
-        
-        int n=robot.size(), m=factory.size();
-        memset(dp, -1, sizeof(dp));
-        return f(n-1, m-1, 0, robot, factory);
+
+        int n = robot.size();
+        int m = factory.size();
+
+        dp.assign(n, vector<long long>(m, -1));
+
+        return solve(0, 0, robot, factory);
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
